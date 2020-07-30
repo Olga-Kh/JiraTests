9c7c10b1dadef34d37e20f8cb7e931a80d2c2936
Olga-Kh <khobina.olga@gmail.com>
2020-07-30 17:19:39 +0300

    added failing test

--- src/main/java/pages/ViewIssuePage.java
+++ src/main/java/pages/ViewIssuePage.java
@@ -15,8 +15,8 @@ public class ViewIssuePage {
   private WebDriverWait wait = null;
   private By issuesDropdown = By.id("find_link");
   private By firstIssueInDropdown = By.xpath("//div[@id='issues_history_main']//ul/li[1]");
-  private By issueType = By.id("type-val");
-  private By ticketNumber = By.id("key-val");
+  //private By issueType = By.id("type-val");
+  private By ticketNumber = By.id("key-val1");
 
 
   public ViewIssuePage(WebDriver driver) {
